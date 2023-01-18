a8ffae047385bf97b306faff58325ce1c7c8370a
blkurd <blkurd@hotmail.com>
2023-01-17 23:09:51 -0500

    first commit

---
+++ models/car.js
@@ -0,0 +1,24 @@
+
+//// Our schema and model for the car resource          
+const mongoose = require('mongoose') // import mongoose
+
+// we'll destructure the Schema and model functions from mongoose
+const { Schema, model } = mongoose
+
+// cars schema
+const carSchema = new Schema({
+    name: String,
+    color: String,
+    readyToRide: Boolean
+})
+
+// make the car model
+// the model method takes two arguments
+// the first is what we call our model
+// the second is the schema used to build the model
+const Car = model('Car', carSchema)
+
+
+// Export our Model 
+
+module.exports = Car
---
+++ server.js
@@ -0,0 +1,94 @@
+// Import Dependencies
+
+const express = require("express"); // import the express framework
+const mongoose = require("mongoose"); // import the mongoose library
+const morgan = require("morgan"); // import the morgan request logger
+require("dotenv").config(); // Load my ENV file's variables
+const path = require("path"); // import path module
+
+// Import Our Models
+
+const Car = require("./models/car");
+
+// Database Connection
+
+// this is where we will set up our inputs for our database connect function
+const DATABASE_URL = process.env.DATABASE_URL;
+// here is our DB config object
+const CONFIG = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+// establish our database connection
+mongoose.connect(DATABASE_URL, CONFIG);
+
+// Tell mongoose what to do with certain events
+// what happens when we open, diconnect, or get an error
+mongoose.connection
+  .on("open", () => console.log("Connected to Mongoose"))
+  .on("close", () => console.log("Disconnected from Mongoose"))
+  .on("error", (err) => console.log("An error occurred: \n", err));
+
+// Create our Express App Object //
+
+const app = express();
+
+
+// Middleware    
+
+// middleware runs before all the routes.
+// every request is processed through our middleware before mongoose can do anything with it
+app.use(morgan('tiny')) // this is for request loggging, the 'tiny' argument declares what size of morgan log to use
+app.use(express.urlencoded({ extended: true })) //this parses urlEncoded request bodies(useful for POST and PUT requests)
+app.use(express.static('public')) // this serves static files from the 'public' folder
+app.use(express.json()) // parses incoming request payloads with JSON
+
+//// Routes
+
+app.get('/', (req, res) => {
+    res.send('Server is live, ready for requests')
+})
+
+// we're going to build a seed route
+// this will seed the database for us with a few starter resources
+// There are two ways we will talk about seeding the database
+// First -> seed route, they work but they are not best practices
+// Second -> seed script, they work and they ARE best practices
+app.get('/cars/seed', (req, res) => {
+    // array of starter resources(fruits)
+    const cars = [
+        { name: 'Ferari', color: 'red', readyToRide: true },
+        { name: 'Volvo', color: 'black', readyToRide: true },
+        { name: 'BMW', color: 'blue', readyToRide: false }
+       
+    ]
+        // then we delete every fruit in the database(all instances of this resource)
+        Car.deleteMany({})
+        .then(() => {
+            // then we'll seed(create) our starter cars
+            Car.create(cars)
+                // tell our db what to do with success and failures
+                .then(data => {
+                    res.json(data)
+                })
+                .catch(err => console.log('The following error occurred: \n', err))
+        })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+// Server Listener             
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`Now listening to the sweet sounds of port: ${PORT}`))
+
+// END
