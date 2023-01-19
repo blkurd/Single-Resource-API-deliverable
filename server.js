508c8242dac46150209af57aaef41c611d649b40
blkurd <blkurd@hotmail.com>
2023-01-19 00:40:50 -0500

    Done

--- server.js
+++ server.js
@@ -55,13 +55,13 @@ app.get("/", (req, res) => {
 // First -> seed route, they work but they are not best practices
 // Second -> seed script, they work and they ARE best practices
 app.get("/cars/seed", (req, res) => {
-  // array of starter resources(fruits)
+  // array of starter resources(cars)
   const cars = [
     { name: "Ferari", color: "red", readyToRide: true },
     { name: "Volvo", color: "black", readyToRide: true },
     { name: "BMW", color: "blue", readyToRide: false },
   ];
-  // then we delete every fruit in the database(all instances of this resource)
+  // then we delete every car in the database(all instances of this resource)
   Car.deleteMany({}).then(() => {
     // then we'll seed(create) our starter cars
     Car.create(cars)
@@ -74,9 +74,9 @@ app.get("/cars/seed", (req, res) => {
 });
 
 // INDEX route
-// Read -> finds and displays all fruits
+// Read -> finds and displays all cars
 app.get("/cars", (req, res) => {
-  // find all the fruits
+  // find all the cars
   Car.find({})
     // send json if successful
     .then((cars) => {
@@ -95,13 +95,55 @@ app.post("/cars", (req, res) => {
   const newCar = req.body;
 
   Car.create(newCar)
-    // send a 201 status, along with the json response of the new fruit
+    // send a 201 status, along with the json response of the new car
     .then((car) => {
       res.status(201).json({ car: car.toObject() });
     })
     // send an error if one occurs
     .catch((err) => console.log(err));
 });
+
+
+// PUT route
+// Update -> updates a specific car
+// PUT replaces the entire document with a new document from the req.body
+// PATCH is able to update specific fields at specific times, but it requires a little more code to 
+// ensure that it works properly, so we'll use that later
+app.put('/cars/:id', (req, res) => {
+    // save the id to a variable for easy use later
+    const id = req.params.id
+    // save the request body to a variable for easy reference later
+    const updatedCar = req.body
+    // we're going to use the mongoose method:
+    // findByIdAndUpdate
+    // eventually we'll change how this route works, but for now, we'll do everything in one shot, with findByIdAndUpdate
+    Car.findByIdAndUpdate(id, updatedCar, { new: true })
+        .then(car => {
+            console.log('the newly updated car', car)
+            // update success message will just be a 204 - no content
+            res.sendStatus(204)
+        })
+        .catch(err => console.log(err))
+})
+
+
+// DELETE route
+// Delete -> delete a specific car
+app.delete('/cars/:id', (req, res) => {
+    // get the id from the req
+    const id = req.params.id
+    // find and delete the car
+    Car.findByIdAndRemove(id)
+        // send a 204 if successful
+        .then(() => {
+            res.sendStatus(204)
+        })
+        // send an error if not
+        .catch(err => console.log(err))
+})
+
+
+
 
 // SHOW route
 // Read -> finds and displays a single resource
@@ -110,7 +152,7 @@ app.get("/cars/:id", (req, res) => {
   const id = req.params.id;
   // use a mongoose method to find using that id
   Car.findById(id)
-    // send the fruit as json upon success
+    // send the car as json upon success
     .then((car) => {
       res.json({ car: car });
     })
