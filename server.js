444864bb057b7fdb5bca0d93127bf19ac56190f2
blkurd <blkurd@hotmail.com>
2023-01-21 01:47:50 -0500

    middlewear worked

--- server.js
+++ server.js
@@ -1,33 +1,32 @@
-// Import Dependencies
-
-const express = require("express"); // import the express framework
-const morgan = require("morgan"); // import the morgan request logger
-require("dotenv").config(); // Load my ENV file's variables
-const path = require("path"); // import path module
+/////////////////////////////////////
+//// Import Dependencies         ////
+/////////////////////////////////////
+const express = require('express') // import the express framework
+const morgan = require('morgan') // import the morgan request logger
+require('dotenv').config() // Load my ENV file's variables
+const path = require('path') // import path module
 const CarRouter = require('./controllers/carControllers')
 // const UserRouter = require('./controllers/userControllers')
-// const middleware = require('./utils/middleware')
-// Import Our Models
+// const CommentRouter = require('./controllers/commentControllers')
+const middleware = require('./utils/middleware')
 
-// const Car = require("./models/car");
+/////////////////////////////////////
+//// Create our Express App Object //
+/////////////////////////////////////
+const app = express()
 
-// // Create our Express App Object //
-
-const app = express();
-
-// Middleware
-
+/////////////////////////////////////
+//// Middleware                  ////
+/////////////////////////////////////
 // middleware runs before all the routes.
 // every request is processed through our middleware before mongoose can do anything with it
-app.use(morgan("tiny")); // this is for request loggging, the 'tiny' argument declares what size of morgan log to use
-app.use(express.urlencoded({ extended: true })); //this parses urlEncoded request bodies(useful for POST and PUT requests)
-app.use(express.static("public")); // this serves static files from the 'public' folder
-app.use(express.json()); // parses incoming request payloads with JSON
+// our middleware is now processed by a function in the utils directory. This middleware function takes one argument, app, and processes requests through our middleware
+middleware(app)
 
-// middleware(app)
 
-// Routes        
-
+/////////////////////////////////////
+//// Routes                      ////
+/////////////////////////////////////
 app.get('/', (req, res) => {
     res.send('Server is live, ready for requests')
 })
@@ -35,15 +34,14 @@ app.get('/', (req, res) => {
 // This is now where we register our routes, this is how server.js knows to send the correc response. 
 // app.use, when we register a route, needs two arguments
 // the first arg is the base URL, second arg is the file to use.
-app.use('/cars', CarRouter)
+app.use('/fruits', CarRouter)
+// app.use('/comments', CommentRouter)
 // app.use('/users', UserRouter)
 
-
-
-// Server Listener
-const PORT = process.env.PORT;
-app.listen(PORT, () =>
-  console.log(`Now listening to the sweet sounds of port: ${PORT}`)
-);
+/////////////////////////////////////
+//// Server Listener             ////
+/////////////////////////////////////
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`Now listening to the sweet sounds of port: ${PORT}`))
 
 // END
